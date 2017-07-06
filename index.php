f60d19fe3a8da24513875c69f915ebc0b34b8cc1
Antonio <antonio.plantas@gmail.com>
2017-07-06 09:57:34 +0200

    make it better

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Merienda" rel="stylesheet">
+        
+        
+        <style type="text/css">
+            
+            
+            body{
+                
+                background-color:black;
+                
+            }
+    
+            h1{
+                
+                margin-bottom: 30px;
+                margin-top: 30px;
+                color: white;
+                font-family: 'Pacifico', cursive;
+                
+            }
+            
+            h4{
+                margin-bottom: 20px;
+                color: white;
+                font-family: 'Pacifico', cursive;
+                
+            }
+            
+             ul { display:table; margin:0 auto;}
+            
+            .facts{
+                margin-top: 100px;
+            }
+            
+            .wiki{
+                
+                margin-top: 100px;
+                font-family: 'Merienda', cursive;
+            }
+            
+            .credits{
+                
+                color: white;
+                font-family: 'Merienda', cursive;
+            }
+            
+            li{
+                font-family: 'Merienda', cursive;
+            }
+            
+            h3{
+                
+                font-family: 'Pacifico', cursive;
+            }
+        
+
+        </style>
+    
+
+        
+        <title>Steve Jobs Tribute Page</title>
+    </head>
+    
+    
+    
+    <body>
+        
+        <div class="fluid-container">
+
+
+
+            <h1 class="text-center">Steve Jobs Tribute Page</h1>
+            <h4 class="text-center"><em>"Stay hungry stay foolish"</em></h4>
+            
+        <div class="row text-center slika">
+            
+            
+            <div class="col-md-12">
+            
+                <img src="jobs.jpg" class="img-thumbnail" alt="Cinque Terre" width="1110" height="1110">
+               
+            
+            </div>
+            
+  
+       
+        </div>
+            
+            <div class="container">
+             <div class="jumbotron">
+                <h3 class="text-center">Life and event chronology:</h3>
+                 <ul>
+                <li>1955 -   Born in San Francisco</li>
+                <li>1968  -  13-year-old Steve Jobs calls up Bill Hewlett and gets a summer job at the HP factory</li>
+                <li>1976 -   Apple Computer Inc. is incorporated by Steve Jobs, Steve Wozniak and Ron Wayne</li>
+                     <li> 1980 -  Apple goes public, increasing Steve Jobs' net worth from dozens of millions of dollars to over $200 million</li>
+                     <li>1989 -  Steve Jobs is named 'Entrepreneur of the decade' by Inc. magazine</li>
+                     <li>2007 - iPhone is released in USA</li>
+                     <li>2011 - Steve Jobs died at home, surrounded by his family</li>
+                     
+                
+                 </ul>
+                 
+                 <h3 class="text-center facts">Most interesting facts:</h3>
+                 
+                 <ul>
+                 
+                     <li>Steve Jobs was adopted shortly after being born.</li>
+                     <li>Jobs was a pescetarian, meaning he ate no meat except for fish.</li>
+                     <li>Jobs has called experimenting with LSD as "one of the two or three most important things I have done in my life."</li>
+                     <li>Jobs had romantic relationships with Joan Baez and Diane Keaton.</li>
+                     <li>Apple co-founder Steve Wozniak notes that Jobs never learned how to code.</li>
+                     <li>Jobs harbored an intense dislike for PCs, and is quoted as saying to one friend, "I'd rather sell dog shit than PCs."</li>
+                 
+                 
+                 </ul>
+                 
+                 <h6 class="text-center wiki">More info on:<a href="https://en.wikipedia.org/wiki/Steve_Jobs" target="_blank">Wikipedia page Steve Jobs</a></h6>
+        
+                 </div>
+            </div>
+            
+            <div class="credits row text-center">
+            
+            <div class="col-md-12">
+                
+                <p>Created by Antonio Plantas</p>
+                
+                
+                </div>
+            
+            </div>
+            
+            
+       
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+    </body>
+    
+    
+
+    
+
+
+</html>
